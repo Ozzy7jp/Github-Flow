789a5d6decba0b5a2ee9afc55708bd4179b8f533
ozzy7 <oguchansmember@gmail.com>
2024-02-23 19:08:20 +0900

    All Completedgit add *

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ $("#addTodo").click(function () {
 
     $("input").val("");
 });
+
+$(document).on("change", "input[type=checkbox]", function() {
+    if($(this).is(":checked")){
+        $(this).parent().css("text-decoration", "line-through");
+    } else{
+        $(this).parent().css("text-decoration", "none");
+    }
+})
