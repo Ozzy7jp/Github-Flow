a200ca8c94ccae5b36ede7a04a90079e44119bc0
ozzy7 <oguchansmember@gmail.com>
2024-02-23 19:17:53 +0900

    Fixed chr color problem

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ $("#addTodo").click(function () {
 $(document).on("change", "input[type=checkbox]", function() {
     if($(this).is(":checked")){
         $(this).parent().css("text-decoration", "line-through");
+        $(this).parent().css("color", "#ccc")
     } else{
         $(this).parent().css("text-decoration", "none");
+        $(this).parent().css("color", "#000")
     }
 })
